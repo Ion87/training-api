7a15eb76c40f77553ebd23a19b522b947dfa9386
Veaceslav Codrean <veaceslav.codrean@endava.com>
2020-11-06 16:19:57 +0200

    [MDDIP006-9535] - project configure structure : add spring boot starter web service, data jpa and flyway dependency

---
+++ src/test/java/com/endava/trainingapi/TrainingApiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.endava.trainingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
