2f3f87f0a777571027d4e172eaba415d5e346ebd
icovdii <iulita.covdii@endava.com>
2020-12-07 17:52:03 +0200

    [MDDIP006-9615] - create community repository

--- src/main/java/com/endava/trainingapi/repository/CommunityRepository.java
+++ src/main/java/com/endava/trainingapi/repository/CommunityRepository.java
@@ -3,8 +3,10 @@ package com.endava.trainingapi.repository;
 import com.endava.trainingapi.model.Community;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CommunityRepository extends JpaRepository<Community, UUID> {
 }
